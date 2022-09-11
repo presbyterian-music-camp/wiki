048cd03368454ef17abdfd25025f5d6bd416d99b
Dan <dansonlinepresence@gmail.com>
2022-09-10 22:29:04 -0400

    initial commit

---
+++ do.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python3
+
+#===== imports =====#
+import argparse
+import copy
+import datetime
+import os
+import re
+import subprocess
+import sys
+
+#===== args =====#
+parser = argparse.ArgumentParser()
+parser.add_argument('--docker-run', '--dkrr', action='store_true')
+parser.add_argument('--docker-cp-certs', '--dkrc', action='store_true')
+args = parser.parse_args()
+
+#===== consts =====#
+DIR = os.path.dirname(os.path.realpath(__file__))
+
+#===== setup =====#
+os.chdir(DIR)
+
+#===== helpers =====#
+def blue(text):
+    return '\x1b[34m' + text + '\x1b[0m'
+
+def timestamp():
+    return '{:%Y-%m-%d %H:%M:%S.%f}'.format(datetime.datetime.now())
+
+def invoke(
+    *args,
+    popen=False,
+    no_split=False,
+    out=False,
+    quiet=False,
+    **kwargs,
+):
+    if len(args) == 1 and not no_split:
+        args = args[0].split()
+    if not quiet:
+        print(blue('-'*40))
+        print(timestamp())
+        print(os.getcwd()+'$', end=' ')
+        if any([re.search(r'\s', i) for i in args]):
+            print()
+            for i in args: print(f'\t{i} \\')
+        else:
+            for i, v in enumerate(args):
+                if i != len(args)-1:
+                    end = ' '
+                else:
+                    end = ';\n'
+                print(v, end=end)
+        if kwargs: print(kwargs)
+        if popen: print('popen')
+        print()
+    if kwargs.get('env') != None:
+        env = copy.copy(os.environ)
+        env.update(kwargs['env'])
+        kwargs['env'] = env
+    if popen:
+        return subprocess.Popen(args, **kwargs)
+    else:
+        if 'check' not in kwargs: kwargs['check'] = True
+        if out: kwargs['capture_output'] = True
+        result = subprocess.run(args, **kwargs)
+        if out:
+            result = result.stdout.decode('utf-8')
+            if out != 'exact': result = result.strip()
+        return result
+
+#===== main =====#
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit()
+
+if args.docker_run:
+    invoke('docker compose up -d')
+
+if args.docker_cp_certs:
+    invoke('docker cp -L /etc/letsencrypt/live/wiki.presbyterianmusiccamp.ca/privkey.pem pmc-wiki-main:/wiki/privkey.pem')
+    invoke('docker cp -L /etc/letsencrypt/live/wiki.presbyterianmusiccamp.ca/fullchain.pem pmc-wiki-main:/wiki/fullchain.pem')
+    invoke('docker exec -u root pmc-wiki-main chown node /wiki/privkey.pem')
+    invoke('docker exec -u root pmc-wiki-main chown node /wiki/fullchain.pem')
